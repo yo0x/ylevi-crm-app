5c3f4557c4350bc31408382f1c42416bf2d432ba
Yo0x <6yonor@gmail.com>
2021-04-04 20:34:16 +0000

    working on

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminPortalComponent } from './admin-portal/admin-portal.component';
+import { ProjectsViewComponent } from './projects-view/projects-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPortalComponent,
+    ProjectsViewComponent
   ],
   imports: [
     BrowserModule,
